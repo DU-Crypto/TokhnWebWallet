abada2a695d2544abf7c862fff07cc1f91fb3803
Mike Flot <mic615@gmail.com>
2018-05-08 21:04:17 -0500

    accidently removed welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+<head>
+        <title> TokhnW </title>
+    </head>
+    <body>
+        <h1>Wallet</h1>
+    </body>
 <div style="width: 500px; margin-left: 20px;">
 <load-wallet-component></load-wallet-component>
 <add-wallet-component></add-wallet-component>
 
-
-
 </div>
  @endsection('content')
-  
