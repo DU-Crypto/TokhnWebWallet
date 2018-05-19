dec5a8e8cec87a953ee55a654a06e70791f93662
Mike Flot <mic615@gmail.com>
2018-05-19 10:40:13 -0500

    got wallet generation right. added proto file

--- resources/assets/js/components/TokhnWComponent.vue
+++ resources/assets/js/components/TokhnWComponent.vue
@@ -24,6 +24,7 @@
                         <b>Ballance: </b> 0
                         <br>
 
+
                         <!-- Total Balance USD: ${{(gasPrice * parseFloat(balance.assets.GAS.balance) + neoPrice * parseInt(balance.assets.NEO.balance)).toFixed(2) }}
                         <input placeholder="NEP-5 Script Hash" class="form-control" v-model="scriptHash">
                         <!-- <button class="btn btn-sm btn-success" v-on:click="addToken()">Add Token</button> -->
@@ -103,20 +104,114 @@
             }
           }
         },
-        // created(){
-        //     if ('serviceWorker' in navigator) {
-        //     window.addEventListener('load', function() {
-        //       navigator.serviceWorker.register('/background.js').then(function(registration) {
-        //
-        //       }, function(err) {
-        //         // registration failed :(
-        //         console.log('ServiceWorker registration failed: ', err);
-        //       });
-        //     });
-        //   }
-        //
-        // },
-        methods:{
+        methods: {
+          createWallet: function() {
+            const SHA2 = require("sha2");
+            const crypto = require('crypto');
+
+              const EC = require('elliptic').ec;
+
+              const RIPEMD160 = require('ripemd160');
+
+              const bs58 = require('bs58');
+
+              const buffer = require('buffer');
+
+              const ec = new EC('p192');
+
+
+
+              function hash(data) {
+
+                  return SHA2.sha512_t(256, data);
+
+              } // A small function I create
+              const addrVer = Buffer.alloc(1, 0x02); // 0x00 P2PKH Mainnet, 0x6f P2PKH Testnet
+
+              const wifByte = Buffer.alloc(1, 0x02); // 0x80 Mainnet, 0xEF Testnet
+              var key = ec.genKeyPair();
+              var privateKey = key.getPrivate().toString('hex');
+              var pubPoint = key.getPublic();
+              var x = pubPoint.getX(); // elliptic x
+              var y = pubPoint.getY(); // elliptic y
+              // var bufPrivKey = Buffer.from(privKey, 'hex');
+              // var wifBufPriv = Buffer.concat([wifByte, bufPrivKey], wifByte.length + bufPrivKey.length);
+              // var wifHashFirst = hash(wifBufPriv);
+              // var wifHashSecond = hash(wifHashFirst);
+              // var wifHashSig = wifHashSecond.slice(0, 4);
+              // var wifBuf = Buffer.concat([wifBufPriv, wifHashSig], wifBufPriv.length + wifHashSig.length);
+              //
+              // var wifFinal = bs58.encode(wifBuf);
+               var publicKey = pubPoint.encode('hex');
+
+              var publicKeyInitialHash = hash(Buffer.from(publicKey, 'hex'));
+
+              var publicKeyRIPEHash = new RIPEMD160().update(Buffer.from(publicKeyInitialHash, 'hex')).digest('hex');
+
+              var hashBuffer = Buffer.from(publicKeyRIPEHash, 'hex');
+
+              var concatHash = Buffer.concat([addrVer, hashBuffer], addrVer.length + hashBuffer.length);
+
+              var hashExtRipe = hash(concatHash);
+
+              var hashExtRipe2 = hash(hashExtRipe);
+
+              var hashSig = hashExtRipe2.slice(0, 4);
+
+              var bitcoinBinaryStr = Buffer.concat([concatHash, hashSig], concatHash.length + hashSig.length);
+              var tokhnAddress = bs58.encode(Buffer.from(bitcoinBinaryStr));
+              var utxos = [];
+              console.log(" Address : %s", tokhnAddress.toString('hex'));
+              var file = '/tmp/Wallet.json'
+                   var wallet = {"address": tokhnAddress,"publicKey":publicKey,"privateKey":privateKey,"utxos":utxos};
+                var walletJSON =JSON.stringify(wallet);
+
+            console.log(walletJSON);
+            this.save('Wallet.json',walletJSON)
+          },
+          save: function(filename, data) {
+            var blob = new Blob([data], {type: 'text/csv'});
+            if(window.navigator.msSaveOrOpenBlob) {
+                window.navigator.msSaveBlob(blob, filename);
+            }
+            else{
+                var elem = window.document.createElement('a');
+                elem.href = window.URL.createObjectURL(blob);
+                elem.download = filename;
+                document.body.appendChild(elem);
+                elem.click();
+                document.body.removeChild(elem);
+            }
+              // var key = ec.genKeyPair();
+              // var privateKey = key.getPrivate();
+              // var publicKey = key.getPublic();
+              // var x = publicKey.getX();
+              // var y = publicKey.getY();
+//               var publicKeyBytes = integerToBytes(x,32) //integerToBytes is found in bitcoinjs-lib/src/ecdsa.js
+//               publicKeyBytes = publicKeyBytes.concat(integerToBytes(y,32))
+//               publicKeyBytes.unshift(0x02)
+// var publicKeyHex = Crypto.util.bytesToHex(publicKeyBytes)
+              // var keybytes = publicKey.encode('hex'); // case 1
+              // var int32x = new Int32Array.from(x.toArrayLike(Buffer));
+              // var int32y = new Int32Array.from(y);
+              // var pubstr = {
+              //     x: x.toString('hex'),
+              //     y: y.toString('hex')
+              // }; // case 2
+              // //var pubbuff = {
+              // //    x: x.toBuffer(),
+              // //    y: y.toBuffer()
+              // //}; // case 3
+              // var pubarr = {
+              //     x : x.toArrayLike(Buffer),
+              //     y : y.toArrayLike(Buffer),
+              //      int32 :new Int32Array(x ),
+              //      int32 : new Int32Array(y )
+              //
+              // }; // case 3
+              //
+              // console.log(key);
+            },
           handleFileChange(e) {
             this.$emit('input', e.target.files[0])
             var input = e.target;
@@ -128,178 +223,46 @@
             console.log(reader.result.substring(0, 200));
           };
           reader.readAsText(input.files[0]);
-          var wallet = JSON.parse( text);
-          // get num networks somehow
-          var Token = this.Token ;
-                      Token.address= wallet.address;
-                      Token.publicKey=wallet.publicKey;
-                      Token.privateKey=wallet.privateKey;
-                      Token.utxos=wallet.utxos;
+          var wallet = JSON.parse(text);
+          var account = this.Account;
+                      account.address= wallet.address;
+                      account.publicKey=wallet.publicKey;
+                      account.privateKey=wallet.privateKey;
+                      account.utxos=wallet.utxos;
           console.log(wallet);
 
-          // createAccount:function(){
-
-            // this.account =new wallet.Account(this.privateKey).decrypt(this.phrase);
-            // var balance = new wallet.Balance({net: 'MainNet', address: this.account.address})
-            // var that = this;
-            // api.neonDB.getBalance('MainNet',this.account.address).then(obj => {
-            //   that.balance = obj;
-            //   api.cmc.getPrice('GAS').then(res => {
-            //     that.gasPrice = res;
-            //   })
-            //   api.cmc.getPrice('NEO').then(res => {
-            //     that.neoPrice = res;
-            //   })
-
-              this.loggedin = true;
-            },
-            createWallet: function() {
-                var EC = require('elliptic').ec;
-                var ec = new EC('p192');
-                var key = ec.genKeyPair();
-                var privateKey = key.getPrivate();
-                var publicKey = key.getPublic();
-                var x = publicKey.getX();
-                var y = publicKey.getY();
-
-
-                // Public Key MUST be either:
-                // 1) '04' + hex string of x + hex string of y; or
-                // 2) object with two hex string properties (x and y); or
-                // 3) object with two buffer properties (x and y)
-                var address = publicKey.encode('hex'); // case 1
-
-                var pubstr = {
-                    x: x.toString('hex'),
-                    y: y.toString('hex')
-                }; // case 2
-                //var pubbuff = {
-                //    x: x.toBuffer(),
-                //    y: y.toBuffer()
-                //}; // case 3
-                var pubarr = {
-                    x: x.toArrayLike(Buffer),
-                    y: y.toArrayLike(Buffer)
-                }; // case 3
-
-                // Import public key
-                var key = ec.keyFromPublic(address, 'hex');
-
-                var utxos = [];
-                console.log(privateKey);
-
-                var file = '/tmp/Wallet.json'
-                     var wallet = {"address": address,"publicKey":publicKey,"privateKey":privateKey,"utxos":utxos};
-                  var walletJSON =JSON.stringify(wallet);
-
-              console.log(walletJSON);
-              this.save('Wallet.json',walletJSON)
-            },
-            save: function(filename, data) {
-              var blob = new Blob([data], {type: 'text/csv'});
-              if(window.navigator.msSaveOrOpenBlob) {
-                  window.navigator.msSaveBlob(blob, filename);
-              }
-              else{
-                  var elem = window.document.createElement('a');
-                  elem.href = window.URL.createObjectURL(blob);
-                  elem.download = filename;
-                  document.body.appendChild(elem);
-                  elem.click();
-                  document.body.removeChild(elem);
-              }
-          }
-
+         //  axios.post('/wallet', {
+         // params: {
+         //   account: wallet
+         //   }
+         // })
+         //  .then(function (response) {
+         //   console.log(response);
+         // })
+         // .catch(function (error) {
+         //   console.log(error);
+         // });
 
           }
         }
+      }
+      </script>
 
-          // addToken:function(){
-          //   /*
-          //   const scriptHashes = [
-          //     '34579e4614ac1a7bd295372d3de8621770c76cdc',
-          //     'b951ecbbc5fe37a9c280a76cb0ce0014827294cf',
-          //     'ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9'
-          //
-          //   ];
-          //
-          //   */
-          //   var that = this;
-          //   var getName = { scriptHash:this.scriptHash, operation: 'name', args: [] }
-          //   var getDecimals = { scriptHash:this.scriptHash, operation: 'decimals', args: [] }
-          //   var getSymbol = { scriptHash:this.scriptHash, operation: 'symbol', args: [] }
-          //   var getTotalSupply = { scriptHash:this.scriptHash, operation: 'totalSupply', args: [] }
-          //   var getBalance = {
-          //     scriptHash: this.scriptHash,
-          //     operation: 'balanceOf',
-          //     args:[that.Neon.u.reverseHex(that.Neon.u.str2hexstring(that.account.address))]
-          //   };
-          //   var script = that.Neon.create.script([getName, getDecimals, getSymbol, getTotalSupply,getBalance]);
-          //   Neon.rpc.Query.invokeScript(script)
-          //   .execute('http://seed3.neo.org:20332')
-          //   .then(res => {
-          //     console.log(res) // You should get a result with state: "HALT, BREAK"
-          //     if(res.result.state != "FAULT, BREAK"){
-          //       var token ={name:that.Neon.u.hexstring2str(res.result.stack[0].value), decimals: res.result.stack[1].value,symbol:that.Neon.u.hexstring2str(res.result.stack[2].value),
-          //       totalSupply:that.Neon.u.hexstring2str(res.result.stack[3].value),balance:that.Neon.u.hexstring2str(res.result.stack[4].value)};
-          //       that.tokens.push(token);
-          //     }
-          //
-          //   });
+      <style scoped>
+      .file-select > .select-button {
+        padding: 1rem;
 
-        //  },
-          // sendAssetPrompt: function(){
-          //   console.log('sendAsset Clicked!');
-          //   $("#assetModal").modal()
-          // },
-          // sendAsset: function(){
-          //           // We want to send 1 NEO and 1 GAS to ALq7AWrhAueN6mJNqk6FHJjnsEoPRytLdW
-          // if(this.neoSend >0 && this.gasSend > 0){
-          //   var intent = api.makeIntent({NEO:this.neoSend, GAS:this.gasSend}, this.sendAddr)
-          //
-          // }
-          // else if(this.neoSend >0 && this.gasSend <= 0){
-          // var intent = api.makeIntent({NEO:this.neoSend}, this.sendAddr)
-          //
-          // }
-          // else if(this.neoSend <=0 && this.gasSend > 0){
-          //   var intent = api.makeIntent({GAS:this.gasSend}, this.sendAddr)
-          //
-          // }
-          // else{
-          //   alert('Must send an asset bigger than 0!')
-          // }
-          // console.log(intent) // This is an array of 2 Intent objects, one for each asset
-          //
-          // const config = {
-          //   net: 'MainNet', // The network to perform the action, MainNet or TestNet.
-          //   address: this.account.address,  // This is the address which the assets come from.
-          //   privateKey: this.account.privateKey,
-          //   intents: intentz
-          // }
-          // this.Neon.sendAsset(config)
-          // .then(config => {
-          //   alert("Success txid: "+config.response.txid)
-          //   var that = this;
-    //         api.neonDB.getBalance('MainNet',this.account.address).then(obj => {
-    //           that.balance = obj;
-    //       })
-    //       .catch(config => {
-    //         console.log(config)
-    //       })
-    //     })
-    //   },
-    //       claimGas: function(){
-    //
-    //         this.Neon.claimGas({net: 'MainNet',address:this.account.address,privateKey: this.account.privateKey})
-    //         .then(config => {
-    //           console.log(config.response)
-    //           alert('Gas CLAIMED!')
-    //         })
-    //         .catch(config => {
-    //           console.log(config)
-    //         })
-    //       }
-    //     }
-    // }
+        color: white;
+        background-color: #2EA169;
+
+        border-radius: .3rem;
+
+        text-align: center;
+        font-weight: bold;
+      }
+
+      .file-select > input[type="file"] {
+        display: none;
+      }
+      </style>
 </script>
